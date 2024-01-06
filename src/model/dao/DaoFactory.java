225eac20baa74f8a0dc30b3231c7acde16725d8f
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-01-06 11:06:58 -0300

    implementação e teste do departamento

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,16 @@
 package model.dao;
 
 import db.DB;
-import model.dao.impl.SellerDaoJdbc;
+import model.dao.impl.DepartmentDaoJDBC;
+import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
+
 	public static SellerDao createSellerDao() {
-			return new SellerDaoJdbc(DB.getConnection());
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 }
